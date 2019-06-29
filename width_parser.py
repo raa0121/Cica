24858b2a3e666bb3ae5d89e3047b1a266a5fcee7
miiton <468745+miiton@users.noreply.github.com>
2019-06-30 00:07:26 +0900

    ✨ 文字幅を返すためのclass追加

---
+++ width_parser.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+
+# 0: ambi, 1: half, 2: wide
+# glyphs = [
+#         {0xe0b3: 0},
+#         {0xe0b3: 0},
+#         {0xe0b3: 0},
+#         {0xe0b3: 0},
+# ]
+
+class WidthParser:
+    """グリフの幅をunicode.orgのデータから判定する
+
+    Examples
+    --------
+    >>> wp = WidthParser()
+    >>> wp.width(0x3000)
+    2
+    """
+    def __init__(self):
+        self.width_type = {
+                'A': 0,
+                'H': 1, 'N': 1, 'Na': 1,
+                'F': 2, 'W': 2,
+        }
+        self.dictionary = {}
+        self.execute()
+
+
+    def parse_line(self, line):
+        a = line.split(";")
+        u = a[0].split("..")
+        width = self.width_type[a[1]]
+        start = int(u[0], 16)
+        if len(u) == 2:
+            finish = int(u[1], 16)
+            for i in range(start, finish):
+                self.dictionary[i] = width
+        else:
+            self.dictionary[start] = width
+
+
+    def execute(self):
+        """EastAsianWidth.txtをパースする
+        """
+        pattern = '^([^ ]+) *#.*$'
+        regex = re.compile(pattern)
+        data = open("source/EastAsianWidth.txt", "r")
+
+        # 一行ずつ読み込んでは表示する
+        for line in data:
+            match = regex.match(line)
+            if match:
+                self.parse_line(match.group(1))
+
+        # ファイルをクローズする
+        data.close()
+
+    def width(self, uni):
+        return self.dictionary[uni]
+
+
+
+if __name__ == '__main__':
+    wp = WidthParser()
+    print(wp.width(0x3000))
