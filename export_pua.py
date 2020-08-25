48da83580ce08738509cd4f2d7e52050f489a17c
tsukkee <takayuki0510@gmail.com>
2020-08-25 16:42:31 +0900

    Add missing characters (0xe0a0 and 0xf4a8)
    This commit will close #64 .

--- export_pua.py
+++ export_pua.py
@@ -5,5 +5,5 @@ import fontforge
 
 cica = fontforge.open('./dist/Cica-Regular.ttf')
 for g in cica.glyphs():
-    if g.isWorthOutputting and g.encoding > 0xe0a0 and g.encoding < 0xf4a8:
+    if g.isWorthOutputting and g.encoding >= 0xe0a0 and g.encoding <= 0xf4a8:
         print('<div class="glyph"><div class="glyphUnicode">%s</div><div class="glyphDisplay">&#%s;</div></div>' % (hex(g.unicode), g.encoding))
